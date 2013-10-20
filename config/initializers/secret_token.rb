450b9c65fcd5f8b8fdf58470e38738a210609311
Patrick Trillo <patarrr@Patricks-MacBook-Pro.local>
2013-10-19 17:37:29 -0700

    Initial rails directories and files setup

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+FuelEd::Application.config.secret_key_base = 'd7ab05b20affe36fdfd1563eabe508317a3340db63ca495be0fd357e0e26f58fc02e1f00bb8397309461b8f6f43dfa94144c659f8c87711551789c0600c85b1c'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+FuelEd::Application.config.session_store :cookie_store, key: '_FuelEd_session'
