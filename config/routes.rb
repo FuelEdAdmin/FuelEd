b76d12206774db0ff3d488af2cf784cab05516a9
saasbook <saasbook@saasbook.(none)>
2013-10-26 17:56:03 -0700

    changed view to haml, moved style outside of view code, changed some colors, put image logo on page

--- config/routes.rb
+++ config/routes.rb
@@ -5,6 +5,7 @@ FuelEd::Application.routes.draw do
 
   # You can have the root of your site routed with "root"
   root 'index#index'
+  #resources :index
 
   # Example of regular route:
   #   get 'products/:id' => 'catalog#view'
