e9536fe450f303ec85d6df326b8a2f146dd70e02
saasbook <saasbook@saasbook.(none)>
2013-10-27 18:49:08 -0700

    updated appointments cucumber tests, added a path

--- features/step_definitions/index_steps.rb
+++
@@ -1,9 +0,0 @@
-# Add a declarative step here for populating the DB with movies.
-
-Given /I am not logged in/ do
-  flunk "Unimplemented"
-end
-
-Given /I am logged in as "(.*)"/ do
-  flunk "Unimplemented"
-end
--- features/support/paths.rb
+++ features/support/paths.rb
@@ -15,6 +15,8 @@ module NavigationHelpers
 
     when /^the home\s?page$/
       '/'
+    when /^the appointments homepage$/
+      '/appointments/'
 
     # Add more mappings here.
     # Here is an example that pulls values out of the Regexp:
