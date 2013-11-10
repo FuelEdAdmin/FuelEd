a324e99f29cf4413cbe81aa6f8ddd67789fa1dce
lewisren <lewisren@gmail.com>
2013-11-09 18:56:33 -0800

    edited gemfiles

--- app/controllers/identities_controller.rb
+++ app/controllers/identities_controller.rb
@@ -2,4 +2,10 @@ class IdentitiesController < ApplicationController
 	def new
 		@identity = env['omniauth.identity']
 	end
+
+	def index
+		if not current_user.nil?
+          redirect_to "/appointments"
+        end
+    end
 end
