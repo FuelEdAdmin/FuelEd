1cc4bc1f3d6ce1641091f2664e4c6a6defd5bd0d
lewisren <lewisren@gmail.com>
2013-11-13 13:31:48 -0800

    fixed some whitespace

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -4,19 +4,19 @@ class SessionsController < ApplicationController
   end
 
 	def create
-    user = User.from_omniauth(env["omniauth.auth"], params["rank"])
-                puts user.id
+    	user = User.from_omniauth(env["omniauth.auth"], params["rank"])
+        puts user.id
 		session[:user_id] = user.uid
 		redirect_to appointments_path, notice: "Signed in!"
 	end
 
 	def destroy
-  	session[:user_id] = nil
-  	redirect_to root_url notice: "Signed out!"
+  		session[:user_id] = nil
+  		redirect_to root_url notice: "Signed out!"
 	end
 
 	def failure
-		redirect_to root_url, alert: "Authentication failed, please try again."
+        redirect_to root_url, alert: "Authentication failed, please try again."
 	end
 
 end
