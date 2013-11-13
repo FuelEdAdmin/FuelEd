6cb273deaae6103cc619844d84456d5bdc87c1e3
lewisren <lewisren@gmail.com>
2013-11-13 13:02:12 -0800

    end of merging all branches

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -11,8 +11,8 @@ class SessionsController < ApplicationController
 	end
 
 	def destroy
-  		session[:user_id] = nil
-  		redirect_to root_url notice: "Signed out!"
+  	session[:user_id] = nil
+  	redirect_to root_url notice: "Signed out!"
 	end
 
 	def failure
