900adf9ce9c28c69d99930ea03d78b1b6700382f
David Le Brun <dlebrun@berkeley.edu>
2013-11-14 17:05:13 -0800

    changed signup process to reflect admin's creation of intern/client accounts (i.e. don't automatically sign in as newly created account, redirect to userinfo page after new creation, etc.  ALSO school model to include full address information

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -3,12 +3,16 @@ class UsersController < ApplicationController
 	end
 
 	def show
+<<<<<<< HEAD
 		@user_show_id = current_user.nil? ? "content" : "calendar"
+=======
+>>>>>>> changed signup process to reflect admin's creation of intern/client accounts (i.e. don't automatically sign in as newly created account, redirect to userinfo page after new creation, etc.  ALSO school model to include full address information
 		@user = User.find_by_uid(params[:id])
 	end
 
 	def edit
     		@user = User.find_by_uid(params[:id])
+<<<<<<< HEAD
 		if current_user.nil?
 			redirect_to root_url
 		end
@@ -17,6 +21,8 @@ class UsersController < ApplicationController
 			flash.keep
 			redirect_to "/appointments"
 		end
+=======
+>>>>>>> changed signup process to reflect admin's creation of intern/client accounts (i.e. don't automatically sign in as newly created account, redirect to userinfo page after new creation, etc.  ALSO school model to include full address information
   	end
 	
 	def update
