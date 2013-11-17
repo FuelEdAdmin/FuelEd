e9e031419b1402662a4cc0e664fa3a67546d8fb3
Stephanie Tung <stepheee@gmail.com>
2013-11-16 21:06:17 -0800

    moved headers to layout/application.css, current /users/id view, when not logged in, sucks, but it'll do for now

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -3,11 +3,20 @@ class UsersController < ApplicationController
 	end
 
 	def show
+		@user_show_id = current_user.nil? ? "content" : "calendar"
 		@user = User.find_by_uid(params[:id])
 	end
 
 	def edit
     		@user = User.find_by_uid(params[:id])
+		if current_user.nil?
+			redirect_to root_url
+		end
+		if @user.uid != current_user.uid
+			flash[:warning] = "cannot edit other users' profile"
+			flash.keep
+			redirect_to "/appointments"
+		end
   	end
 	
 	def update
