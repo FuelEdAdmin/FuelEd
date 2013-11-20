34a7dc1be7558c9e4ea12c331a10441ad80ba7eb
David Le Brun <dlebrun@berkeley.edu>
2013-11-20 13:02:45 -0800

    Update application_controller to search by UID

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -8,7 +8,7 @@ class ApplicationController < ActionController::Base
   	def current_user
   		puts "SESSION[:user_id] IS    :"
   		puts session[:user_id]
-  		@current_user ||= User.find(session[:user_id]) if session[:user_id]
+  		@current_user ||= User.find_by_uid(session[:user_id]) if session[:user_id]
   	end
   	helper_method :current_user
 end
