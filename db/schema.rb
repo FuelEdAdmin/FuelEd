e481ab480d681853168ae065b082afedb8512c3b
David Le Brun <dlebrun@berkeley.edu>
2013-11-20 10:48:36 -0800

    added association lines

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,6 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-
 ActiveRecord::Schema.define(version: 20131115004216) do
 
   create_table "appointments", force: true do |t|
