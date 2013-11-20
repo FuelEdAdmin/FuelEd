3edfdf2858ea0055eef9f90cf36a152371ee3ca6
David Le Brun <dlebrun@berkeley.edu>
2013-11-20 11:03:12 -0800

    resolving merge conflict (db & master)

--- db/schema.rb
+++ db/schema.rb
@@ -16,9 +16,6 @@ ActiveRecord::Schema.define(version: 20131115004216) do
   create_table "appointments", force: true do |t|
     t.string   "client"
     t.string   "intern"
-    t.string   "date"
-    t.integer  "start"
-    t.integer  "end"
     t.datetime "created_at"
     t.datetime "updated_at"
     t.string   "school"
