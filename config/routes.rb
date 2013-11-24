99d41596d2a92fbefddb149e7d99820f77c83923
sebanie <stephanie.tung@berkeley.edu>
2013-11-23 17:47:20 -0800

    hopefully makes different portal title show up

--- config/routes.rb
+++ config/routes.rb
@@ -26,6 +26,7 @@ FuelEd::Application.routes.draw do
   match "/auth/identity/register" => "identities#new", via: :post
   match "/auth/failure" => "sessions#failure", via: :get
   match "/signout" => "sessions#destroy", :as => :signout, via: :get
+  #match "/appointments/past.html" => "appointments@past_appts"
   
   scope '/admins' do
     resources :identities
