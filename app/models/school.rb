561c904f908672d8415844962cbc0f4ad16e867e
David Le Brun <dlebrun@berkeley.edu>
2013-11-24 17:12:07 -0800

    changed school/appointment model (associaltions)

--- app/models/school.rb
+++ app/models/school.rb
@@ -1,5 +1,6 @@
 class School < ActiveRecord::Base
 	has_and_belongs_to_many :users
+	has_many :appointments
 	#has_and_belongs_to_many :appointments
 	
 	validates_presence_of :name, :length => { :maximum => 50 }
