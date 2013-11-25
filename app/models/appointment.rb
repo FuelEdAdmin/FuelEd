f68a339d508135c803f54345f7dfe3cf7f82cc2b
lewisren <lewisren@gmail.com>
2013-11-24 16:03:03 -0800

    validation incomplete

--- app/models/appointment.rb
+++ app/models/appointment.rb
@@ -1,11 +1,8 @@
 class Appointment < ActiveRecord::Base
-
 	# Validates for presence 
 	validates :client, :presence => true, :length => { :maximum => 50 }
 	validates :intern, :presence => true, :length => { :maximum => 50 }
-	validates :date, :presence => true
 	validates_numericality_of :start
   	validates_numericality_of :end, :greater_than => :start
-	validates :start, :presence => true, :inclusion => 0..2359
-	validates :end, :presence => true, :inclusion => 0..2359
+	validates :room, :presence => true, :length => { :maximum => 25 }
 end
--- app/models/school.rb
+++ app/models/school.rb
@@ -1,2 +1,9 @@
 class School < ActiveRecord::Base
+	validates_presence_of :name, :length => { :maximum => 50 }
+	validates_presence_of :country, :length => { :maximum => 50 }
+	validates_presence_of :state, :length => { :maximum => 20 }
+	validates_presence_of :county, :length => { :maximum => 50 }
+	validates_presence_of :city, :length => { :maximum => 50 }
+	validates_presence_of :address, :length => { :maximum => 50 } 
+	validates :zipcode, presence: true, numericality: { only_integer: true }, :inclusion => 0..100000
 end
