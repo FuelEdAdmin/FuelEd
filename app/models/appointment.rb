841c79605897857140ae31fad6771a464bbf5429
Patrick Trillo <patarrr@Patricks-MacBook-Pro.local>
2013-11-24 18:24:55 -0800

    working on forms

--- app/models/appointment.rb
+++ app/models/appointment.rb
@@ -13,7 +13,6 @@ class Appointment < ActiveRecord::Base
 	# validates :room, :presence => true, :length => { :maximum => 25 }
 
 
-
 	def self.populate_minutes()
 		array = []
 		61.times do |x|
--- db/schema.rb
+++ db/schema.rb
@@ -11,11 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-<<<<<<< HEAD
 ActiveRecord::Schema.define(version: 20131125015408) do
-=======
-ActiveRecord::Schema.define(version: 20131125002834) do
->>>>>>> modified appointments form
 
   create_table "appointments", force: true do |t|
     t.string   "client"
