2deb1a8458184542ee195ebd60fdbbbbfd95652f
Stephanie Tung <stepheee@gmail.com>
2013-12-04 23:37:09 -0800

    past appointments and my booked appointments query works

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -17,8 +17,18 @@ class ApplicationController < ActionController::Base
       #@current_user = User.find_by_uid(session[:user_id]) if session[:user_id]
   	end
 
+    def booked_appts
+         current_date = Time.new
+	 if @current_user.rank == "client"
+	    	@myappts = Appointment.where(["start >= ? and client = ?", "#{current_date}", "#{current_user.uid}"])
+	 elsif @current_user.rank == "intern"
+	    	@myappts = Appointment.where(["start >= ? and client != ?", "#{current_date}", ""])
+	 end
+         @myappts
+    end
+
     def admin_exists
       @admin_exists = User.find_by_rank("admin")
     end
-    helper_method :current_user, :admin_exists
+    helper_method :current_user, :admin_exists, :booked_appts
 end
