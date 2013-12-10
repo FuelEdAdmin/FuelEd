4ea87a2d4429f6889556ea42681536d5ebec7913
Patrick Trillo <patarrr@Patricks-MacBook-Pro.local>
2013-12-09 17:36:59 -0800

    added password changes

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,3 +1,4 @@
+require 'bcrypt'
 class UsersController < ApplicationController
 	def new
 	end
@@ -18,12 +19,39 @@ class UsersController < ApplicationController
 			redirect_to "/appointments"
 		end
   	end
+
+  	def change_password 
+  		@user = User.find_by_uid(current_user.uid)
+  	end
 	
 	def update
 		
     @user = User.find_by_uid(params[:id])
-    @user.update_attributes!(params[:user].permit(:name, :email, :bio))
-    redirect_to user_path(@user)
+
+    user_identity = Identity.find_by_email(@user.email)
+    unencrypted_password = params[:password_digest].to_s
+    unencrypted_password_confirmation = params[:password_digest_confirmation].to_s
+
+    if unencrypted_password == unencrypted_password_confirmation
+
+	    password_digest = BCrypt::Password.create(unencrypted_password)
+	    user_identity.password_digest = password_digest;
+	    user_identity.save!
+
+	    if !params[:user].nil?
+	    	@user.update_attributes!(params[:user].permit(:name, :email, :bio))
+	    end
+
+	    flash[:notice] = "User information successfully updated."
+	    flash.keep
+	    redirect_to user_path(@user)
+	else
+		flash[:warning] = "Password and confirmation did not match."
+		flash.keep
+		redirect_to change_password_path
+	end
+
+
 
 	end
 end
