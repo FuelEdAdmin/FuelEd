66c40fc45f7b28839e23e4b19f0d578eb2e335be
Stephanie Tung <stepheee@gmail.com>
2013-12-09 23:26:23 -0800

    added migration to add user_id to identities

--- db/migrate/20131210072312_add_user_id_to_identities.rb
+++ db/migrate/20131210072312_add_user_id_to_identities.rb
@@ -1,4 +1,5 @@
 class AddUserIdToIdentities < ActiveRecord::Migration
   def change
+    add_column(:identities, :user_id, :integer)
   end
 end
--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20131205061934) do
+ActiveRecord::Schema.define(version: 20131210072312) do
 
   create_table "appointments", force: true do |t|
     t.string   "client"
@@ -32,6 +32,7 @@ ActiveRecord::Schema.define(version: 20131205061934) do
     t.string   "password_digest"
     t.datetime "created_at"
     t.datetime "updated_at"
+    t.integer  "user_id"
   end
 
   create_table "schools", force: true do |t|
